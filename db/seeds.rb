04ef95baf8d917fd827b67127d2e8d993a8d28bb
github username <github email address>
2020-01-25 18:48:25 -0800

    working with join table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ Familyjoin.delete_all
 
 family = Family.create(surname: 'Capp')
 
-rel1 = Relationship.create(relation_type: 'spouse', description: 'Andrew-Sharon')
+#rel1 = Relationship.create(relation_type: 'spouse', description: 'Andrew-Sharon')
 rel2 = Relationship.create(relation_type: 'child', description: 'Andrew and Sharon family')
-rel3 = Relationship.create(relation_type: 'parent', description: 'parents Andrew and Sharon')
+#rel3 = Relationship.create(relation_type: 'parent', description: 'parents Andrew and Sharon')
 
 familymembers1 = [
     {first_name: 'Andrew', last_name: 'Capp', gender: 'male'},
@@ -20,20 +20,23 @@ familymembers1.each do |member|
     a.save
 end
 
-#manually update and save relationships for family members that are spouses
+#manually create and link relationships for family members that are spouses
 
     a = Familymember.find_by(first_name: "Andrew")
-    rel1.familymembers << Familymember.find_by(first_name: "Sharon")
-    rel1.save
-    a.relationships << rel1
+    binding.pry
+    rel = Relationship.create(relation_type: 'spouse', description: 'Sharon is my spouse')
+    rel.familymembers << Familymember.find_by(first_name: "Sharon")
+    rel.save
+    a.relationships << rel
     #a.relationships << rel3
     a.save
 
-    a = Familymember.find_by(first_name: "Sharon")
-    rel1.familymembers << Familymember.find_by(first_name: "Andrew")
-    rel1.save
-    a.relationships << rel1
-    a.save
+    b = Familymember.find_by(first_name: "Sharon")
+    rel = Relationship.create(relation_type: 'spouse', description: 'Andrew is my spouse')
+    binding.pry
+    rel.save
+    b.relationships << rel
+    b.save
 
 familymembers2 = [
     {first_name: 'Amber', last_name: 'Burns', gender: 'female'},
