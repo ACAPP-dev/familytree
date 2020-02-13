a4fc10b4cc255c6135b3be81999b3d028d7636d0
ACAPP-dev <acapp909@gmail.com>
2020-02-13 08:47:15 -0800

    small clean up of files

--- app/Not Used Items/Not Used familyjoin.rb
+++
@@ -1,7 +0,0 @@
-#class Familyjoin < ActiveRecord::Base
-
- # belongs_to :familymember
- # belongs_to :relationship
-  #belongs_to :dad, class_name: "Familymember", foreign_key: "dad_id"
-  #belongs_to :mom, class_name: "Familymember", foreign_key: "mom_id"
-#end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ActiveRecord::Base
     
     belongs_to :familymember
-    belongs_to :related_familymember, class_name: "Familymember"#, "foreign_key: "related_familymember_id"
+    belongs_to :related_familymember, class_name: "Familymember"
 
 end
